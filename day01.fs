f4647e88d9d46f77f5eebaab0ab6efd0ece6af93
Joel Kaasinen <joel.kaasinen@gmail.com>
2025-12-01 13:47:17 +0200

    day01: broken part2

--- day01.fs
+++ day01.fs
@@ -64,4 +64,43 @@ Create line-buffer 256 allot
     repeat
 
     cr cr zeros .
-    ;
+;
+
+: turn2 ( old step -- new zero-crossings )
+    +
+    dup ( unmodded unmodded )
+    100 / abs ( unmodded zero-crossings )
+    \ check if we landed on 0
+    over 0 = if 1 + then
+    \ TODO: if we started from zero, don't count first cross into negative side
+    swap 100 mod swap ( modded zero-crossings )
+;
+
+: tick2 ( n ccc len -- n )
+    parse ( old step )
+    ." STEP"
+    dup .
+    turn2 ( new crossings )
+
+    \ store zero crossings
+    zeros + to zeros
+
+    ." TO"
+    dup .
+    ." ZEROS"
+    zeros .
+    .\" \n"
+;
+
+: part2
+    s" day01.input" r/o open-file throw to infile
+    50
+    begin
+        getline
+        .s
+        while
+        tick2
+    repeat
+
+    cr cr zeros .
+;
