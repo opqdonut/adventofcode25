a2a40f6798e8aae0f05e6a4cb7023c616a536a78
Joel Kaasinen <joel.kaasinen@gmail.com>
2025-12-03 08:59:32 +0200

    day03: polish

--- day03.fs
+++ day03.fs
@@ -6,57 +6,38 @@ Create line-buffer 256 allot
     line-buffer 256 infile read-line throw
 ;
 
-: str-max ( ccc len start -- char )
-    -1 -rot '.' -rot
+: max-char ( end start -- pointer )
+    dup 1- '.' 2swap ( initiali initialc end start )
     ?do
-        ( ccc maxi maxc )
-        third i + c@
-        ( ccc maxi maxc c' )
-        over over ( ccc maxi maxc c' max c' )
-        < if -rot drop drop i swap else drop then
+        dup i c@ ( maxi maxc maxc c )
+        < if 2drop i i c@ then
     loop
-    ( ccc maxi maxc )
-    rot drop
+    drop
 ;
 
-: eq2 ( x1 y1 x2 y2 -- f )
-    rot ( x1 x2 y2 y1 )
-    = ( x1 x2 yf )
-    -rot ( yf x1 x2 )
-    = ( yf xf )
-    and
-;
-
-: str-max-t
-    assert( s" 01234321" 0 str-max 4 '4' eq2 )
-    assert( s" 01234321" 5 str-max 5 '3' eq2 )
-    assert( s" 1119171" 0 str-max 3 '9' eq2 )
-    assert( s" 1119171" 4 str-max 5 '7' eq2 )
-    assert( s" 12346" 0 str-max 4 '6' eq2 )
-    assert( s" 12346" 3 str-max 4 '6' eq2 )
+: max-char-t
+    assert( s" 012345" line-buffer swap cmove
+    line-buffer 6 + line-buffer max-char c@ '5' = )
 ;
 
 : digit-to-int ( c -- c )
     '0' -
 ;
 
-: -rot4
-    3 roll 3 roll 3 roll
-;
-
 : max-joltage ( ccc len -- n )
-    \ first digit picked must not be last digit of string
-    over over 1- 0 ( ccc len ccc len-1 0 )
-    str-max ( ccc len maxi maxc )
+    over + swap ( end start )
+    \ first digit must not be last digit of string
+    over 1- swap ( end end-1 start )
+    max-char ( end max-pointer )
+    dup c@ digit-to-int -rot ( digit1 end max-pointer )
     \ second digit comes after first digit
-    -rot4 ( maxc ccc len maxi )
-    1+ str-max ( maxc maxi' maxc' )
-    swap drop ( maxc maxc' )
-    digit-to-int swap digit-to-int 10 * +
+    1+ max-char ( digit1 max-pointer )
+    c@ digit-to-int ( digit1 digit 2 )
+    swap 10 * +
 ;
 
 : max-joltage-t
-    assert( s" 987654321111111" max-joltage 98 = )
+    assert( s" 987654321111111" max-joltage .s 98 = )
     assert( s" 811111111111119" max-joltage .s 89 = )
 ;
 
@@ -78,25 +59,27 @@ Create line-buffer 256 allot
 0 Value current
 
 : add-digit ( c -- )
-    digit-to-int current 10 * + to current
+    c@ digit-to-int current 10 * + to current
 ;
 
-: max-joltage-2 {: str n -- n :}
+: max-joltage-2 {: start n | end -- n :}
     0 to current
-    str n 11 - 0 str-max ( maxi maxc )
-    add-digit ( maxi )
-    str n 10 - rot 1+ ( str n maxi+1 ) str-max ( maxi' maxc' )
-    add-digit
-    str n 9 - rot 1+ str-max add-digit
-    str n 8 - rot 1+ str-max add-digit
-    str n 7 - rot 1+ str-max add-digit
-    str n 6 - rot 1+ str-max add-digit
-    str n 5 - rot 1+ str-max add-digit
-    str n 4 - rot 1+ str-max add-digit
-    str n 3 - rot 1+ str-max add-digit
-    str n 2 - rot 1+ str-max add-digit
-    str n 1 - rot 1+ str-max add-digit
-    str n 0 - rot 1+ str-max add-digit
+    start n + to end
+
+    end 11 - start max-char ( max-pointer )
+    dup add-digit ( max-pointer )
+
+    end 10 - swap 1+ max-char dup add-digit
+    end 9 - swap 1+ max-char dup add-digit
+    end 8 - swap 1+ max-char dup add-digit
+    end 7 - swap 1+ max-char dup add-digit
+    end 6 - swap 1+ max-char dup add-digit
+    end 5 - swap 1+ max-char dup add-digit
+    end 4 - swap 1+ max-char dup add-digit
+    end 3 - swap 1+ max-char dup add-digit
+    end 2 - swap 1+ max-char dup add-digit
+    end 1 - swap 1+ max-char dup add-digit
+    end 0 - swap 1+ max-char dup add-digit
     drop current
 ;
 
@@ -122,7 +105,7 @@ Create line-buffer 256 allot
 \ answer 169512729575727
 
 : tests
-    str-max-t
+    max-char-t
     max-joltage-t
     max-joltage-2-t
 ;
